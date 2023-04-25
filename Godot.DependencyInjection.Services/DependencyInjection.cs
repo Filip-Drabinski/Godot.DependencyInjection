a55cd5044371ed510a13f9031e474782c2f32682
Filip Drabinski <58997858+Filip-Drabinski@users.noreply.github.com>
2023-04-25 21:35:59 +0200

    Add dependency registration extension method for services.

---
+++ Godot.DependencyInjection.Services/DependencyInjection.cs
@@ -0,0 +1,19 @@
+﻿using Godot.DependencyInjection.Services.Input;
+
+namespace Microsoft.Extensions.DependencyInjection
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddGodotServices(this IServiceCollection services)
+        {
+            services.AddGodotInputService();
+            return services;
+        }
+
+        public static IServiceCollection AddGodotInputService(this IServiceCollection services)
+        {
+            services.AddSingleton<IInputService, InputService>();
+            return services;
+        }
+    }
+}
