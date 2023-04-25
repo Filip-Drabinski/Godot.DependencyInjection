c0fbce44dd6498e0242561a8a40410b01d5c061b
Filip Drabinski <58997858+Filip-Drabinski@users.noreply.github.com>
2023-04-25 19:11:28 +0200

    Deprecate DependencyInjectionNodeObserver, create DependencyInjectionManagerNode and IServicesConfigurator
    Deprecates the 'DependencyInjectionNodeObserver' class and introduces the 'DependencyInjectionManagerNode' class and 'IServicesConfigurator' interface. This change splits the role of the 'DependencyInjectionNodeObserver' into two separate components: the 'DependencyInjectionManagerNode' class for managing dependency injection, and the 'IServicesConfigurator' interface for registering services. From now on, dependencies will be registered using all classes implementing the 'IServicesConfigurator' interface with instances in the scene tree at the moment of scene creation.

---
+++ Godot.DependencyInjection/IServicesConfigurator.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace Godot.DependencyInjection
+{
+    public interface IServicesConfigurator
+    {
+        /// <summary>
+        /// Configures the services to be used in the dependency injection container.
+        /// </summary>
+        /// <param name="services">The service collection to add services to.</param>
+        void ConfigureServices(IServiceCollection services);
+    }
+}
